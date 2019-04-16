8858ff854c4a1413b4a02f01658f8b3a4acc5bfe
Alexey L <alex10sx@rambler.ru>
2019-04-16 22:10:51 +0300

    Updates

---
+++ precache-manifest.d2bcc872040f234fb1a846aaa5e813a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "775aaaf2e9d3b022ff77",
+    "url": "/static/js/main.a570ff9f.chunk.js"
+  },
+  {
+    "revision": "37e023aa40eac8f6e9b7",
+    "url": "/static/js/2.5be5d0c7.chunk.js"
+  },
+  {
+    "revision": "775aaaf2e9d3b022ff77",
+    "url": "/static/css/main.8e031d83.chunk.css"
+  },
+  {
+    "revision": "46aed5365632f82758d99004ac9b949b",
+    "url": "/index.html"
+  }
+];
